a78ff4ff10bee4f5577073f41111a9c5d0e99aca
user1 <doantam8924@gmail.com>
2019-06-06 18:07:07 +0900

    SP top page

---
+++ project_tam/develop/src/js/main.js
@@ -0,0 +1,14 @@
+$(window).on('resize',function(){
+  resize();
+})
+$(function() {
+  $(".menu_btn").click(function() {
+    if (!$(this).hasClass('is-active')) {
+      $(this).addClass('is-active');
+      $('.cv_nav').addClass('active');
+    } else {
+      $(this).removeClass('is-active');
+      $('.cv_nav').removeClass('active');
+    }
+  });
+});
